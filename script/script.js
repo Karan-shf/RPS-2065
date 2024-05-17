28997a67a94c068465f8519607c0c94ecef83cc0
karan <k.shahkolaee@gmail.com>
2024-05-17 16:07:27 +0330

    added two player mode

--- script/script.js
+++ script/script.js
@@ -1,6 +1,15 @@
 let user_win_count = 0;
 let ai_win_count = 0;
 
+function singleplayermode(){
+    document.getElementById('game-mode').style.display = 'none';
+    document.getElementById('single-player-div').style.display = 'block';
+}
+function multiplayermode(){
+    document.getElementById('game-mode').style.display = 'none';
+    document.getElementById('multi-player-div').style.display = 'block';   
+}
+
 function reset_page() {
 
     document.getElementById('intro-div').style.display = 'block';
@@ -108,6 +117,141 @@ function game() {
 
     } else {
         reset_page();
+    }
+
+}
+
+let player1_choice = 'none';
+let player2_choice = 'none';
+let player1_win_count = 0;
+let player2_win_count = 0;
+
+function render_two_player_result(){
+    document.getElementById('two-result').innerHTML = 'Player 1 --- '+player1_win_count+':'+player2_win_count+' --- Player 2';
+}
+
+
+function win_of_player1(){
+    document.getElementById('two-intro').innerHTML = 'Winner: Player 1!';
+    player1_win_count++;
+    render_two_player_result();
+}
+
+function win_of_player2(){
+    document.getElementById('two-intro').innerHTML = 'Winner: Player 2!';
+    player2_win_count++;
+    render_two_player_result();
+}
+
+function reset_2player_page(){
+    document.getElementById('two-game-button').innerHTML = 'START';
+    document.getElementById('player1-choice').innerHTML = '';
+    document.getElementById('player2-choice').innerHTML = '';
+    document.getElementById('two-intro').innerHTML = 'Rock Paper Scissor';
+    player1_choice = 'none';
+    player2_choice = 'none';
+    document.getElementById('player1-rock').style.border = 'none';
+    document.getElementById('player1-paper').style.border = 'none';
+    document.getElementById('player1-scissor').style.border = 'none';
+    document.getElementById('player2-rock').style.border = 'none';
+    document.getElementById('player2-paper').style.border = 'none';
+    document.getElementById('player2-scissor').style.border = 'none';
+}
+
+function calculateResult(){
+
+    if (player1_choice!='none' && player2_choice!='none') {
+        
+        button = document.getElementById('two-game-button').innerHTML;
+    
+        if (button=='START') {
+
+            document.getElementById('two-game-button').innerHTML = 'PLAY AGAIN';
+    
+            if (player1_choice=="rock" && player2_choice=="paper") {
+                document.getElementById('player1-rock').style.border = 'solid red 3px';
+                document.getElementById('player2-paper').style.border = 'solid red 3px';
+                win_of_player2();
+            } else if(player1_choice=="rock" && player2_choice=="scissor") {
+                document.getElementById('player1-rock').style.border = 'solid red 3px';
+                document.getElementById('player2-scissor').style.border = 'solid red 3px';
+                win_of_player1();
+            } else if(player1_choice=="scissor" && player2_choice=="paper") {
+                document.getElementById('player1-scissor').style.border = 'solid red 3px';
+                document.getElementById('player2-paper').style.border = 'solid red 3px';
+                win_of_player1();
+            } else if(player1_choice=="scissor" && player2_choice=="rock") {
+                document.getElementById('player1-scissor').style.border = 'solid red 3px';
+                document.getElementById('player2-rock').style.border = 'solid red 3px';
+                win_of_player2();
+            } else if(player1_choice=="paper" && player2_choice=="rock") {
+                document.getElementById('player1-paper').style.border = 'solid red 3px';
+                document.getElementById('player2-rock').style.border = 'solid red 3px';
+                win_of_player1();
+            } else if(player1_choice=="paper" && player2_choice=="scissor") {
+                document.getElementById('player1-paper').style.border = 'solid red 3px';
+                document.getElementById('player2-scissor').style.border = 'solid red 3px';
+                win_of_player2();
+            } else if(player1_choice=="paper" && player2_choice=="paper") {
+                document.getElementById('player1-paper').style.border = 'solid red 3px';
+                document.getElementById('player2-paper').style.border = 'solid red 3px';
+                document.getElementById('two-intro').innerHTML = 'Draw!';
+            } else if(player1_choice=="rock" && player2_choice=="rock"){
+                document.getElementById('player1-rock').style.border = 'solid red 3px';
+                document.getElementById('player2-rock').style.border = 'solid red 3px';
+                document.getElementById('two-intro').innerHTML = 'Draw!';
+            } else if(player1_choice=="scissor" && player2_choice=="scissor") {
+                document.getElementById('player1-scissor').style.border = 'solid red 3px';
+                document.getElementById('player2-scissor').style.border = 'solid red 3px';
+                document.getElementById('two-intro').innerHTML = 'Draw!';
+            } else {
+                console.log("Something Went Wrong");
+            }
+        } else {
+            reset_2player_page();
+        }
+    } else {
+        alert('both players have to make a choice')
+    }
+}
+
+document.onkeydown = function player_input(event) {
+
+    let key = event.key;
+
+    switch (key) {
+        case "A": case "a":
+            player1_choice = "paper";
+            break;
+        case "S": case "s":
+            player1_choice = "scissor";
+            break;
+        case "D": case "d":
+            player1_choice = "rock";
+            break;
+        case "ArrowLeft":
+            player2_choice = "paper";
+            break;
+        case "ArrowUp":
+            player2_choice = "scissor";
+            break;
+        case "ArrowRight":
+            player2_choice = "rock";
+            break;
+        default:
+            console.log("Invalid Input!");
+            break;
+    }
+
+    console.log("player 1 choice: " + player1_choice);
+    console.log("player 2 choice: " + player2_choice);
+
+    if (player1_choice != 'none') {
+        document.getElementById('player1-choice').innerHTML = "Player 1 made a choice";
+    }
+    
+    if (player2_choice != 'none') {
+        document.getElementById('player2-choice').innerHTML = "Player 2 made a choice";
     }
 
 }
